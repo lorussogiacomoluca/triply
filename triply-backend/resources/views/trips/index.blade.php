1c1b5f561aad00c908f94be5e5dbc40d78672653
lorussogiacomoluca <lorussogiacomoluca@gmail.com>
2025-10-09 23:04:44 +0200

    INDEX: TRIP

---
+++ triply-backend/resources/views/trips/index.blade.php
@@ -0,0 +1,44 @@
+@extends('layouts.trips')
+@section('title', 'Tutti i viaggi')
+
+@section('content')
+    <div class="container">
+        <h2 class="fs-4 text-secondary my-4">
+            {{ __('Tutti i viaggi') }}
+        </h2>
+        <div class="row justify-content-center">
+            <div class="col">
+            @section('content')
+                <div class="container mt-4">
+                    <h1>Viaggi</h1>
+
+                    <table class="table">
+                        <thead>
+                            <tr>
+                                <th>Titolo</th>
+                                <th>Destinazione</th>
+                                <th>Data Inizio</th>
+                                <th>Prezzo</th>
+                                <th>Azioni</th>
+                            </tr>
+                        </thead>
+                        <tbody>
+                            @foreach ($trips as $trip)
+                                <tr>
+                                    <td>{{ $trip->title }}</td>
+                                    <td>{{ $trip->destination }}</td>
+                                    <td>{{ $trip->start_date }}</td>
+                                    <td>€ {{ $trip->price }}</td>
+                                    <td>
+                                        <a href="{{ route('trips.show', $trip->id) }}" class="btn btn-primary btn-sm">Vedi</a>
+                                    </td>
+                                </tr>
+                            @endforeach
+                        </tbody>
+                    </table>
+                </div>
+            @endsection
+        </div>
+    </div>
+</div>
+@endsection
--- triply-backend/routes/web.php
+++ triply-backend/routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use App\Http\Controllers\ProfileController;
+use App\Http\Controllers\TripController;
 use Illuminate\Container\Attributes\Auth;
 use Illuminate\Support\Facades\Route;
 
@@ -16,6 +17,8 @@ Route::middleware('auth')->group(function () {
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
 });
+
+Route::resource('trips', TripController::class);
 
 
 require __DIR__ . '/auth.php';

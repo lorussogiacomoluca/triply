2cf7d7e3a6c594963c02cd1b3da842005f661f20
lorussogiacomoluca <lorussogiacomoluca@gmail.com>
2025-10-12 17:11:12 +0200

    Category: Migration, Model and Seeder

--- triply-backend/database/seeders/DatabaseSeeder.php
+++ triply-backend/database/seeders/DatabaseSeeder.php
@@ -13,6 +13,6 @@ class DatabaseSeeder extends Seeder
      */
     public function run(): void
     {
-        $this->call(TripsTableSeeder::class);
+        $this->call([CategoriesTableSeeder::class, TripsTableSeeder::class]);
     }
 }
